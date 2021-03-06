6da4bad674c88cd3f4ec1b615fdc5c7dbbd9de84
94p3 <55440753+94p3@users.noreply.github.com>
2021-03-06 20:28:22 +0300

    Create gmsh_create_mesh_from_stl.cpp
    cpp file

---
+++ gmsh_create_mesh_from_stl.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+#include<gmsh.h>
+#include<set>
+
+int main(int argc, char **argv){
+    gmsh::initialize();
+    
+  	gmsh::model::add("t13");
+  	try {
+   		gmsh::merge("1.stl");
+  	} catch(...) {
+   		gmsh::logger::write("Could not load STL mesh: bye!");
+    gmsh::finalize();
+    return 0;
+    }
+     
+    double angle = 40;
+	bool forceParametrizablePatches = false;
+	bool includeBoundary = true;
+	double curveAngle = 180;
+	
+	gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., includeBoundary, 							forceParametrizablePatches);
+     
+    gmsh::model::mesh::createGeometry(); 
+    std::vector<std::pair<int, int> > s;
+  	gmsh::model::getEntities(s, 2);
+  	std::vector<int> sl;
+  	for(auto surf : s) sl.push_back(surf.second);
+  	int l = gmsh::model::geo::addSurfaceLoop(sl);
+  	gmsh::model::geo::addVolume({l});  
+  	
+  	gmsh::model::geo::synchronize();
+  	
+  	bool funny = false; // false;
+  	int f = gmsh::model::mesh::field::add("MathEval");
+  	if(funny)
+    	gmsh::model::mesh::field::setString(f, "F", "2*Sin((x+y)/5) + 3");
+  	else
+   		gmsh::model::mesh::field::setString(f, "F", "4");
+  	gmsh::model::mesh::field::setAsBackgroundMesh(f);
+  	
+  	std::set<std::string> args(argv, argv + argc);
+  	if(!args.count("-nopopup")) gmsh::fltk::run();
+  	
+  	gmsh::finalize();
+ 	return 0;
+}
+                           
+                                      
+                                      
+                                      
