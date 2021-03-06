f46403e9f931707795b7811fc994ea8cbd6b7b16
94p3 <55440753+94p3@users.noreply.github.com>
2021-03-06 20:26:59 +0300

    Create cube.cpp
    the cpp file

---
+++ cube.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+#include<gmsh.h>
+#include<set>
+
+int main(int argc, char **argv){
+    gmsh::initialize();
+    gmsh::model::add("t1");
+    double lc = 1e-2;
+    gmsh::model::geo::addPoint(0, 0, 0, lc, 1);
+    gmsh::model::geo::addPoint(0, 0, 0.1, lc, 2);
+    gmsh::model::geo::addPoint(0, 0.1, 0, lc, 3);
+    gmsh::model::geo::addPoint(0, 0.1, 0.1, lc, 4);
+    gmsh::model::geo::addPoint(0.1, 0, 0, lc, 5);
+    gmsh::model::geo::addPoint(0.1, 0, 0.1, lc, 6);
+    gmsh::model::geo::addPoint(0.1, 0.1, 0, lc, 7);
+    gmsh::model::geo::addPoint(0.1, 0.1, 0.1, lc, 8);
+
+
+    gmsh::model::geo::addLine(1, 2, 1);
+    gmsh::model::geo::addLine(2, 4, 2);
+    gmsh::model::geo::addLine(4, 3, 3);
+    gmsh::model::geo::addLine(3, 1, 4);
+
+    gmsh::model::geo::addLine(5, 6, 5);
+    gmsh::model::geo::addLine(6, 8, 6);
+    gmsh::model::geo::addLine(7, 8, 7);
+    gmsh::model::geo::addLine(7, 5, 8);
+
+
+    gmsh::model::geo::addLine(1, 5, 9);
+    gmsh::model::geo::addLine(2, 6, 10);
+    gmsh::model::geo::addLine(3, 7, 11);
+    gmsh::model::geo::addLine(4, 8, 12);
+
+
+    gmsh::model::geo::addCurveLoop({1,2,3,4}, 1);
+    gmsh::model::geo::addPlaneSurface({1}, 1);
+
+    gmsh::model::geo::addCurveLoop({5, 6, -7, 8}, 2);
+    gmsh::model::geo::addPlaneSurface({2}, 2);
+
+    gmsh::model::geo::addCurveLoop({-1, 9, 5, -10}, 3);
+    gmsh::model::geo::addPlaneSurface({3}, 3);
+
+    gmsh::model::geo::addCurveLoop({2, 12, -6, -10}, 4);
+    gmsh::model::geo::addPlaneSurface({4}, 4);
+
+    gmsh::model::geo::addCurveLoop({11, 7, -12, 3}, 5);
+    gmsh::model::geo::addPlaneSurface({5}, 5);
+
+    gmsh::model::geo::addCurveLoop({11, 8, -9, -4}, 6);
+    gmsh::model::geo::addPlaneSurface({6}, 6);
+    
+    gmsh::model::geo::addSurfaceLoop({1, 2, 3, 4, 5, 6}, 1);
+    gmsh::model::geo::addVolume({1});
+
+
+    gmsh::model::geo::synchronize();
+   	gmsh::model::mesh::generate(3);
+
+    //gmsh::write("t1.msh");
+
+    std::set<std::string> args(argv, argv + argc);
+    if(!args.count("-nopopup")) gmsh::fltk::run();
+	
+	gmsh::finalize();
+    
+    return 0;
+}
